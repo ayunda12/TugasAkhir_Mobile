ec4ce2ec397b6c9cc9c0a2ee00093c9277e9cd24
novitabening <75107750+novitabening@users.noreply.github.com>
2022-01-06 22:52:37 +0700

    Add files via upload

---
+++ app/src/main/java/com/indahayu/aplikasiloginregister/ModelRecycler.java
@@ -0,0 +1,30 @@
+package com.indahayu.aplikasiloginregister;
+
+public class ModelRecycler {
+    private String nama, keterangan, gambar;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+
+    public String getKeterangan() {
+        return keterangan;
+    }
+
+    public void setKeterangan(String keterangan) {
+        this.keterangan = keterangan;
+    }
+
+    public String getGambar() {
+        return gambar;
+    }
+
+    public void setGambar(String gambar) {
+        this.gambar = gambar;
+    }
+}
