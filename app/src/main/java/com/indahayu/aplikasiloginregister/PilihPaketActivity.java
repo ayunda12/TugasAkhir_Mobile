86a169ede3e6bf93c418c9820aa0f15a982c6d0c
novitabening <75107750+novitabening@users.noreply.github.com>
2022-01-06 22:55:15 +0700

    Add files via upload

---
+++ app/src/main/java/com/indahayu/aplikasiloginregister/PilihPaketActivity.java
@@ -0,0 +1,14 @@
+package com.indahayu.aplikasiloginregister;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PilihPaketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pilih_paket);
+    }
+}
