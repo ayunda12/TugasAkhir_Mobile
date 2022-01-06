a91737e5a13519e924724e908a30fd47fbbe84a2
indahayuningtias08 <80672080+indahayuningtias08@users.noreply.github.com>
2022-01-06 23:03:46 +0700

    Add files via upload

---
+++ KonfirmasiBatalPesanActivity.java
@@ -0,0 +1,14 @@
+package com.indahayu.aplikasiloginregister;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KonfirmasiBatalPesanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_konfirmasi_batal_pesan);
+    }
+}
