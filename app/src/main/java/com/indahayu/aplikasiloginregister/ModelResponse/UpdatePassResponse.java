33d87c41bde74ce06bdfb08918302442e8c562de
Ayunda Kusuma Wardani <47249108+ayunda12@users.noreply.github.com>
2022-01-06 23:20:31 +0700

    Add files via upload

---
+++ app/src/main/java/com/indahayu/aplikasiloginregister/ModelResponse/UpdatePassResponse.java
@@ -0,0 +1,28 @@
+package com.indahayu.aplikasiloginregister.ModelResponse;
+
+public class UpdatePassResponse {
+    private String error;
+    private String message;
+
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public UpdatePassResponse(String error, String message) {
+        this.error = error;
+        this.message = message;
+    }
+}
